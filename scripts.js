96290fa2ecfa628c02453d5c09a8f488553a3e6d
Brian Ruiz <brianpehuajo3@hotmail.com>
2023-06-06 17:57:24 -0300

    Primer commit de la calculadora con js & tailwind

---
+++ scripts.js
@@ -0,0 +1,39 @@
+let pantalla = document.querySelector('.pantalla');
+
+document.addEventListener('keydown', (e)=> {
+    if (/\d/.test(e.key) || e.key == "x" || e.key == "-" || e.key == "+") {
+        pantalla.value = pantalla.value + e.key
+    }
+    if (e.key == "Backspace") {
+        borrar()
+    }
+})
+
+function clearAll() {
+    pantalla.value = '';
+}
+
+function borrar() {
+    pantalla.value = pantalla.value.slice(0, -1)
+}
+
+function agregarOperador(operador) {
+    let ultimoCaracter = pantalla.value.slice(-1)
+    if (ultimoCaracter != operador) {
+        pantalla.value = pantalla.value + operador;
+    }
+}
+
+function agregarNumero(numero) {
+    pantalla.value = pantalla.value + numero;
+}
+
+function equal() {
+    try {
+        pantalla.value = math.evaluate(pantalla.value);
+    }
+    catch(e) {
+        console.error(e);
+    }
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["index.html"],
+    theme: {
+      extend: {
+        colors: {
+          'fondo': '#1d1d1d',
+          'color-calculadora': '#F0F8FF'
+        }
+      },
+  
+    },
+    plugins: [],
+  }
